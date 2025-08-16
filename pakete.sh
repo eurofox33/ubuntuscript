b9fc345895e23d30672e70ff985efcb6463ad813
eurofox33 <recherche627251@gmail.com>
2025-08-17 00:33:23 +0200

    Update pakete.sh

--- pakete.sh
+++ pakete.sh
@@ -70,6 +70,22 @@ sudo groupadd docker
 # User in die Docker Gruppe
 sudo usermod -aG docker $USER
 
-# --- Schritt 9: Erfolgsmeldung ---
+# Docker userfähig machen
+newgrp docker
+
+# --- Schritt 9: NVIDIA Container Toolkit ---
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey \
+    | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list \
+    | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' \
+    | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+    
+sudo apt update
+sudo apt install -y nvidia-container-toolkit
+
+# --- Schritt 10: OpenWebUI und Ollama Docker Bundle ---
+docker run -d -p 3000:8080 --gpus=all -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
+
+# --- Schritt 11: Erfolgsmeldung ---
 echo ""
 echo "Installation abgeschlossen! Docker erfolgreich installiert."
