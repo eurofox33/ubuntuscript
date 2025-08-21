9d5f9fa329e27ce1268d71cb8d1a72eea38114cd
eurofox33 <recherche627251@gmail.com>
2025-08-21 11:58:09 +0200

    Update pakete.sh

--- pakete.sh
+++ pakete.sh
@@ -58,13 +58,6 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 
 newgrp docker
-
-# --- Schritt 9: NVIDIA Container Toolkit ---
-curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey \
-    | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
-curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list \
-    | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' \
-    | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
     
 sudo apt update
 sudo apt install -y nvidia-container-toolkit
